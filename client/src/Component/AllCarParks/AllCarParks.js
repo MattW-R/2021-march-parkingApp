d883fa726160263436fdd11c6d0ff97d6b377b89
MattW-R <Matthew.wr89@gmail.com>
2021-06-14 15:05:35 +0100

    Added NoCarParks component for when fetch fails/api request gives success=false

--- client/src/Component/AllCarParks/AllCarParks.js
+++ client/src/Component/AllCarParks/AllCarParks.js
@@ -1,31 +1,38 @@
 import './AllCarParks.css'
 import CarPark from './../CarPark/CarPark'
 import {useEffect, useState} from "react"
+import NoCarParks from '../NoCarParks/NoCarParks'
 
 const AllCarParks = () => {
     const[carParks, setCarParks] = useState([])
-    useEffect(()=>{
-        fetch('localhost:9000/carParks')
-            .then(res=> res.json())
-            .then(data=> {
-                if (data.success){
-                    setCarParks(data.data)
-                }
-            })
-    },[])
-    const displayCarParks = () => {
-        return carParks.map((carPark)=>{
-            return <CarPark key={carPark._id} name={carPark.name} location={carPark.location} totalSpaces={carPark.totalSpaces} hourlyRate={carPark.hourlyRate} />
 
+    const displayCarParks = () => {
+        return carParks.map((carPark) => {
+            return <CarPark key={carPark._id} name={carPark.name} location={carPark.location} totalSpaces={carPark.totalSpaces} hourlyRate={carPark.hourlyRate} />
         })
     }
+
+    useEffect(() => {
+        fetch('https://localhost:9000/carParks')
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    setCarParks(displayCarParks(data.data))
+                } else {
+                    setCarParks(<NoCarParks />)
+                }
+            })
+            .catch(() => setCarParks(<NoCarParks />))
+        // eslint-disable-next-line
+    },[])
+
     return (
         <main>
             <div className="all-car-parks-header">
                 <h1>All car parks</h1>
                 <img src="parking-scout-logo.svg" alt="Parking Scout Logo" />
             </div>
-            {displayCarParks()}
+            {carParks}
         </main>
     )
 }
