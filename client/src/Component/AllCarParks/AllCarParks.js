76ba7d2429e15a746964b6d30d9f337254f6895e
Kashif Ameen <kashu@live.co.uk>
2021-06-14 14:30:28 +0100

    added fetch request to allCarParks

--- client/src/Component/AllCarParks/AllCarParks.js
+++ client/src/Component/AllCarParks/AllCarParks.js
@@ -1,16 +1,31 @@
 import './AllCarParks.css'
 import CarPark from './../CarPark/CarPark'
+import {useEffect, useState} from "react"
 
 const allCarParks = () => {
+    const[carParks, setCarParks] = useState([])
+    useEffect(()=>{
+        fetch('localhost:9000/carParks')
+            .then(res=> res.json())
+            .then(data=> {
+                if (data.success){
+                    setCarParks(data.data)
+                }
+            })
+    },[])
+    const displayCarParks = () => {
+        return carParks.map((carPark)=>{
+            return <CarPark key={carPark._id} name={carPark.name} location={carPark.location} totalSpaces={carPark.totalSpaces} hourlyRate={carPark.hourlyRate} />
+
+        })
+    }
     return (
         <main>
             <div className="all-car-parks-header">
                 <h1>All car parks</h1>
                 <img src="parking-scout-logo.svg" alt="Parking Scout Logo" />
             </div>
-            <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
-            <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
-            <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
+            {displayCarParks()}
         </main>
     )
 }
