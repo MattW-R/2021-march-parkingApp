f184ac288db7e6d8d5bcf8a490921250c92b63b9
MattW-R <Matthew.wr89@gmail.com>
2021-06-14 14:18:26 +0100

    Added styling to AllCarParks, tweaked CarPark styles added logo to all car parks page

--- client/src/Component/AllCarParks/AllCarParks.js
+++ client/src/Component/AllCarParks/AllCarParks.js
@@ -4,6 +4,10 @@ import CarPark from './../CarPark/CarPark'
 const allCarParks = () => {
     return (
         <main>
+            <div className="all-car-parks-header">
+                <h1>All car parks</h1>
+                <img src="parking-scout-logo.svg" alt="Parking Scout Logo" />
+            </div>
             <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
             <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
             <CarPark name="Avon Street" location="Bath BA1 1UF" totalSpaces="1" hourlyRate="3.2" />
