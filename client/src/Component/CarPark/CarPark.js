85e2d2f9350607f64474b2cb7a40c111a52d0358
Kashif Ameen <kashu@live.co.uk>
2021-06-14 12:26:48 +0100

    Used props within CarPark render

--- client/src/Component/CarPark/CarPark.js
+++ client/src/Component/CarPark/CarPark.js
@@ -1,8 +1,13 @@
 import "./CarPark.css"
 
-const carPark= () => {
+const carPark= (props) => {
     return (
-        <article></article>
+        <article>
+            <h3>{props.name}</h3>
+            <p>{props.location}</p>
+            <p>{props.totalSpaces} total spaces</p>
+            <p>£{props.hourlyRate} per hour</p>
+        </article>
     )
 }
 
