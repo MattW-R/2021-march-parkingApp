8f26caa8e72709beda0d3baf2fc90d11748dad9b
Kashif Ameen <kashu@live.co.uk>
2021-06-15 11:40:24 +0100

    linked collections

--- api/Controllers/AvailableCarParkController.js
+++ api/Controllers/AvailableCarParkController.js
@@ -2,21 +2,23 @@ const connectToDb = require('../Services/DbService')
 const carParkService = require('../Services/CarParkService')
 const jsonResponse = require('../Services/JsonResponseService')
 
+
 let getAvailableCarParks = async (req, res) => {
     try {
-        connectToDb(async (collection) => {
-            let carParks = await carParkService.getAllCarParks(collection)
-            if (carParks.length > 0) {
-                let jsonRes = jsonResponse.successful()
-                jsonRes.message = 'Success - car parks located'
-                jsonRes.data = carParks
-                res.json(jsonRes)
-            } else {
-                let jsonRes = jsonResponse.unsuccessful()
-                jsonRes.message = 'There are no car parks found'
-                jsonRes.status = 204
-                res.json(jsonRes)
-            }
+        connectToDb(async (collection, bookingCollection) => {
+            let carParks = await carParkService.getAvailableCarParks(collection, bookingCollection, 1)
+            res.json(carParks)
+            // if (carParks.length > 0) {
+            //     let jsonRes = jsonResponse.successful()
+            //     jsonRes.message = 'Success - car parks located'
+            //     jsonRes.data = carParks
+            //     res.json(jsonRes)
+            // } else {
+            //     let jsonRes = jsonResponse.unsuccessful()
+            //     jsonRes.message = 'There are no car parks found'
+            //     jsonRes.status = 204
+            //     res.json(jsonRes)
+            // }
         })
     } catch (error) {
         let jsonRes = jsonResponse.unsuccessful()
--- api/Services/CarParkService.js
+++ api/Services/CarParkService.js
@@ -1,5 +1,16 @@
 let getAllCarParks = async (collection) => {
     return collection.find({}).toArray()
 }
+let getAvailableCarParks = async (carParkCollection,bookingCollection, duration) => {
+    return bookingCollection.aggregate([{
+        $lookup: {
+            from: 'carParks',
+            localField: 'carParkId',
+            foreignField: '_id',
+            as: 'carPark'
+        }
+    }]).toArray()
 
+}
 module.exports.getAllCarParks = getAllCarParks
+module.exports.getAvailableCarParks = getAvailableCarParks
